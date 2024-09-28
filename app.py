784e45ed030ff2b73f7862a5c303e5249d1f66c3
srrevishnu07 <142157548+srrevishnu07@users.noreply.github.com>
2024-09-28 10:51:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 # Dummy data for the dashboard
 project_info = {
     "project_name": "CI/CD Pipeline Demo",
-    "status": "Running",  # Change to "Stopped" to test different status states
+    "status": "Running",
     "version": "v1.0.0",
     "description": "This is a demo project showcasing CI/CD automation using GitHub Actions and Docker.",
     "owner": "srrevis",
     "deployments": 15,
     "tests_passed": 120,
     "issues_resolved": 45,
-    "progress_percentage": 70  # This is for the progress bar
+    "progress_percentage": 70
 }
 
 @app.route("/")
