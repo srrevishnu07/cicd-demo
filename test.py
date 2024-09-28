3f312595bd5f0ae5250d207fad8c2ef94e1cf117
srrevishnu07 <142157548+srrevishnu07@users.noreply.github.com>
2024-09-28 10:34:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,17 @@
 import pytest
 from app import app
 
-
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
-
-def test_project_dashboard(client):
+def test_dashboard(client):
     response = client.get('/')
-    assert response.status_code == 200
-    json_data = response.get_json()
-    assert json_data["project_name"] == "CI/CD Pipeline Demo"
-    assert json_data["status"] == "Running"
-    assert json_data["version"] == "v1.0.0"
-    assert "description" in json_data
-    assert json_data["owner"] == "srrevis"
+    assert response.status_code == 200  # Check that the page loads successfully
+    assert b"Project Dashboard" in response.data  # Verify the title exists
+    assert b"CI/CD Pipeline Demo" in response.data  # Verify the project name exists
+    assert b"Running" in response.data  # Verify the project status exists
+    assert b"v1.0.0" in response.data  # Verify the project version exists
+    assert b"srrevis" in response.data  # Verify the project owner exists
