152b5b876f0337268a180bcdd79c71c9afe1dba9
srrevishnu07 <142157548+srrevishnu07@users.noreply.github.com>
2024-09-28 10:34:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# A simple route to simulate a project dashboard
+# Dummy data for the dashboard
+project_info = {
+    "project_name": "CI/CD Pipeline Demo",
+    "status": "Running",
+    "version": "v1.0.0",
+    "description": "This is a demo project showcasing CI/CD automation using GitHub Actions and Docker.",
+    "owner": "srrevis"
+}
+
 @app.route("/")
 def project_dashboard():
-    project_info = {
-        "project_name": "CI/CD Pipeline Demo",
-        "status": "Running",
-        "version": "v1.0.0",
-        "description": "This is a demo project showcasing CI/CD automation using GitHub Actions and Docker.",
-        "owner": "srrevis",
-    }
-    return jsonify(project_info)
+    return render_template('dashboard.html', project=project_info)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
